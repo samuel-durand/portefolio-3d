3582213179c2e7966296c5e854dc80688470fb2d
samuel-durand <114388132+samuel-durand@users.noreply.github.com>
2023-05-05 19:35:05 +0200

    update nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,13 +11,29 @@ const Navbar = () => {
   const [active, setActive] = useState("");
   return (
     <nav className={'${styles.paddindX} w-full flex-item-center py-5 fixed top-0 z-20 bg-primary'}>
-      <div className="w-full flex justify-between items-center max-w-7x1 mx-auto">
-        <Link to="/" className="flex items-center gap-2" 
-          onClick={()=>{
+      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+        <Link
+          to='/'
+          className='flex items-center gap-2'
+          onClick={() => {
             setActive("");
-            window.scrollTo(0,0);
-          }}/>
-          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
+          <p className='text-white text-[18px] font-bold cursor-pointer flex '>
+            samuel &nbsp;
+            <span className='sm:block hidden'> | durand</span>
+          </p>
+        </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((Link)=>(
+            <li>
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+
+        </ul>
       </div>
     </nav>
   )

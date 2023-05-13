165cbf7d8d311267dcecf51b5366af3be6341ccb
samuel-durand <114388132+samuel-durand@users.noreply.github.com>
2023-05-13 18:17:29 +0200

    About finish added animation for card and more

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,10 +5,31 @@ import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
 
 const ServiceCard = ({ index, title, icon})  => {
   return (
-    <p>{title}</p>
+    <Tilt className="xs:w-[250px] w-full">
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+      options={{
+        max: 45,
+        scale: 1,
+        speed: 450
+      }}
+      className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+      >
+
+        <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+        <h3 className="text-white text-[20px] font-bold text-center">{title}</h3>
+
+      </div>
+
+      </motion.div>
+    </Tilt>
   )
 }
 
@@ -45,4 +66,4 @@ const About = () => {
   )
 }
 
-export default About;
+export default SectionWrapper (About, "about");
